5008ab9a3a3d6adf890d7e58cd89c31a8c2a4608
russell-leung <russell-leung@comcast.net>
2025-03-30 21:05:47 -0400

    npm i necessary packages

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 import express from "express";
 import "dotenv/config";
-const PORT = process.env.port || 4000;
-const app = express()
-app.get('/', (req, res) => {res.send('Hello World!')})
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-})
+import cors from "cors";
+import session from "express-session";
+import mongoose from "mongoose";
+
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/social-wordle-app"
+mongoose.connect(CONNECTION_STRING);
+
+const app = express();
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.NETLIFY_URL || "http://localhost:5173",
+  })
+);
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET || "social-wordle-app",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.NODE_SERVER_DOMAIN,
+  };
+}
+app.use(session(sessionOptions));
+app.use(express.json());
+
+// Add routes
+
+app.listen(process.env.PORT || 4000);
